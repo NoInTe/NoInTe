e77fb4a21ab5689d15672ff6f07b34c804e0964c
HappyNiceDev <04nicolas.k@gmail.com>
2024-10-06 21:43:11 -0300

    Segunda modificacion

---
+++ js/index.js
@@ -0,0 +1,11 @@
+document.querySelector('.whatsapp-icono').addEventListener('click', function () {
+    window.location.href = 'https://wa.me/';
+});
+
+document.querySelector('.scroll-down-indicator').addEventListener('click', function () {
+    window.scrollBy({
+        top: window.innerHeight,
+        behavior: 'smooth'
+    });
+    document.querySelector('.scroll-down-indicator').classList.add('ocultar');
+});
