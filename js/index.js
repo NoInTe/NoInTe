089906a2d14f7f4fa1fdf6491648c6d105f859c8
HappyNiceDev <04nicolas.k@gmail.com>
2024-10-14 14:45:46 -0300

    Correccion js para que ejecute solo al terminar de cargar el DOM

--- js/index.js
+++ js/index.js
@@ -1,11 +1,14 @@
-document.querySelector('.whatsapp-icono').addEventListener('click', function () {
-    window.location.href = 'https://wa.me/';
-});
+document.addEventListener("DOMContentLoaded", function () {
 
-document.querySelector('.scroll-down-indicator').addEventListener('click', function () {
-    window.scrollBy({
-        top: window.innerHeight,
-        behavior: 'smooth'
+    document.querySelector('.whatsapp-icono').addEventListener('click', function () {
+        window.location.href = 'https://wa.me/';
     });
-    document.querySelector('.scroll-down-indicator').classList.add('ocultar');
-});
+
+    document.querySelector('.scroll-down-indicator').addEventListener('click', function () {
+        window.scrollBy({
+            top: window.innerHeight,
+            behavior: 'smooth'
+        });
+        document.querySelector('.scroll-down-indicator').classList.add('ocultar');
+    });
+}
