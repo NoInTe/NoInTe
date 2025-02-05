e59bb305ecf79a8dfb7859d757217ea0788f81e9
HappyNiceDev <04nicolas.k@gmail.com>
2025-02-05 07:55:03 -0300

    modificaciones

--- js/index.js
+++ js/index.js
@@ -1,115 +1,18 @@
-document.addEventListener("DOMContentLoaded", function () {
-
-  document.querySelector('.whatsapp-icono').addEventListener('click', function () {
-    window.location.href = 'https://wa.me/+5491139243159';
-  });
-
-  // Ocultar preloader después de 500ms
-  setTimeout(() => {
-    document.querySelector('.preloader').classList.add("hidden");
-    document.querySelector('main').style.display = "flex"; // Mostrar contenido
-  }, 1000);
-
-  ajusteContenedorVertical();
-
-});
-
-window.addEventListener("resize", function () {
-
-  ajusteContenedorVertical();
-
-});
-
-
-
-
-function ajusteContenedorVertical(){
-  const servicios = document.querySelector(".servicios");
-  const nosotros = document.querySelector(".nosotros");
-  let bordeInferiorServicios = servicios.offsetTop + servicios.offsetHeight;
-  let bordeInferiorNosotros = bordeInferiorServicios + nosotros.offsetHeight;
-
-  document.documentElement.style.setProperty('--nosotros-top', bordeInferiorServicios + "px");
-  document.documentElement.style.setProperty('--main-height', bordeInferiorNosotros + "px");
-  document.documentElement.style.setProperty('--footer-top', bordeInferiorNosotros + "px");
-};
-
-
-
-
+const servicios = document.querySelector(".servicios");
+const nosotros = document.querySelector(".nosotros");
+// ------------------------------HEADER--------------------------------------------//
 //NAV
 const hamburger = document.querySelector(".hamburger");
 const cabecera = document.querySelector(".cabecera");
 const navMenu = document.querySelector(".nav-links");
 
-// Toggle para abrir y cerrar el menú con el icono de la hamburguesa
-hamburger.addEventListener("click", () => {
-  popup.style.display = 'none';
-  hamburger.classList.toggle("active");
-  cabecera.classList.toggle("desactivado");
-  navMenu.classList.toggle("active");
-
-  let menuOpen = navMenu.classList.contains("active");
-  let newMenuOpenStatus = menuOpen;
-  hamburger.setAttribute("aria-expanded", newMenuOpenStatus);
-});
-
-// Cerrar el menú cuando se haga clic en un enlace dentro del menú
-document.querySelectorAll(".nav-links a").forEach(n =>
-  n.addEventListener("click", () => {
-    hamburger.classList.remove("active");
-    cabecera.classList.remove("desactivado");
-    navMenu.classList.remove("active");
-    hamburger.setAttribute("aria-expanded", "false");
-  })
-);
-
-// Cerrar el menú si se hace clic fuera de él
-document.addEventListener("click", (e) => {
-  if (!hamburger.contains(e.target) && !navMenu.contains(e.target)) {
-    hamburger.classList.remove("active");
-    cabecera.classList.remove("desactivado");
-    navMenu.classList.remove("active");
-    hamburger.setAttribute("aria-expanded", "false");
-  }
-});
-
-// Cerrar el menú al hacer scroll
-window.addEventListener("scroll", () => {
-  hamburger.classList.remove("active");
-  cabecera.classList.remove("desactivado");
-  navMenu.classList.remove("active");
-  hamburger.setAttribute("aria-expanded", "false");
-});
-
-// Desplazamiento con efecto al hacer click en los enlaces
-document.querySelectorAll('.nav-links a').forEach(enlace => {
-  enlace.addEventListener('click', function(e) {
-      e.preventDefault(); // Evita el salto instantáneo
-      const seccion = document.querySelector(this.getAttribute('href'));
-
-      if (seccion) {
-          // Desplazamiento suave
-          window.scrollTo({
-              top: seccion.offsetTop,
-              behavior: "smooth"
-          });
-      }
-  });
-});
-
-
-//TERMINA NAV
-
-
+// ------------------------------SERVICIOS--------------------------------------------//
 // Obtiene todos los botones de "Leer más"
 const leerMasBtns = document.querySelectorAll('.leer-mas-btn');
-
 // Elementos del popup
 const popup = document.getElementById('popup');
 const popupDetails = document.getElementById('popup-details');
 const closeBtn = document.querySelector('.close-btn');
-
 // Datos adicionales para cada card
 const contenidoExtra = {
   1: `
@@ -214,7 +117,105 @@ const contenidoExtra = {
               </p>
   `,
 };
+// ------------------------------NOSOTROS--------------------------------------------//
+/* contenido LINEA DE TIEMPO  */
+const zoomInContents = document.querySelectorAll('.nosotros_lt');
 
+
+
+
+// ----------------------------------EVENTOS DEL DOM-------------------------------------//
+
+document.addEventListener("DOMContentLoaded", function () {
+
+  // Ocultar preloader después de 1000ms
+  setTimeout(() => {
+    document.querySelector('.preloader').classList.add("hidden");
+    document.querySelector('main').style.display = "flex"; // Mostrar contenido
+  }, 1000);
+
+  ajusteContenedorVertical();
+
+});
+
+window.addEventListener("resize", function () {
+
+  ajusteContenedorVertical();
+
+});
+
+
+
+// ----------------------AJUSTE DE LOS CONTENEDORES ABSOLUTOS---------------------------//
+function ajusteContenedorVertical() {
+  let bordeInferiorServicios = servicios.offsetTop + servicios.offsetHeight;
+  document.documentElement.style.setProperty('--nosotros-top', bordeInferiorServicios + "px");
+  let bordeInferiorNosotros = bordeInferiorServicios + nosotros.offsetHeight;
+  document.documentElement.style.setProperty('--footer-top', bordeInferiorNosotros + "px");
+  document.documentElement.style.setProperty('--main-height', bordeInferiorNosotros + "px");
+};
+
+// ----------------------ACCION CLIK SOBRE ICONO WHATSAPP---------------------------//
+document.querySelector('.whatsapp-icono').addEventListener('click', function () {
+  window.location.href = 'https://wa.me/+5491139243159';
+});
+
+// ----------------------TODO LO RELACIONADO AL NAV---------------------------//
+// Toggle para abrir y cerrar el menú con el icono de la hamburguesa
+hamburger.addEventListener("click", () => {
+  popup.style.display = 'none';
+  hamburger.classList.toggle("active");
+  cabecera.classList.toggle("desactivado");
+  navMenu.classList.toggle("active");
+  let menuOpen = navMenu.classList.contains("active");
+  let newMenuOpenStatus = menuOpen;
+  hamburger.setAttribute("aria-expanded", newMenuOpenStatus);
+});
+
+// Cerrar el menú cuando se haga clic en un enlace dentro del menú
+document.querySelectorAll(".nav-links a").forEach(n =>
+  n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    cabecera.classList.remove("desactivado");
+    navMenu.classList.remove("active");
+    hamburger.setAttribute("aria-expanded", "false");
+  })
+);
+
+// Cerrar el menú si se hace clic fuera de él
+document.addEventListener("click", (e) => {
+  if (!hamburger.contains(e.target) && !navMenu.contains(e.target)) {
+    hamburger.classList.remove("active");
+    cabecera.classList.remove("desactivado");
+    navMenu.classList.remove("active");
+    hamburger.setAttribute("aria-expanded", "false");
+  }
+});
+
+// Cerrar el menú al hacer scroll
+window.addEventListener("scroll", () => {
+  hamburger.classList.remove("active");
+  cabecera.classList.remove("desactivado");
+  navMenu.classList.remove("active");
+  hamburger.setAttribute("aria-expanded", "false");
+});
+
+// Desplazamiento con efecto al hacer click en los enlaces
+document.querySelectorAll('.nav-links a').forEach(enlace => {
+  enlace.addEventListener('click', function (e) {
+    e.preventDefault(); // Evita el salto instantáneo
+    const seccion = document.querySelector(this.getAttribute('href'));
+    if (seccion) {
+      // Desplazamiento suave
+      window.scrollTo({
+        top: seccion.offsetTop,
+        behavior: "smooth"
+      });
+    }
+  });
+});
+
+// ----------------------TODO LO RELACIONADO CON SERVICIOS---------------------------//
 // Abre el popup con el contenido correspondiente
 leerMasBtns.forEach((btn) => {
   btn.addEventListener('click', () => {
@@ -237,7 +238,7 @@ window.addEventListener('click', (e) => {
 });
 
 
-
+// ----------------------TODO LO RELACIONADO CON NOSOTROS---------------------------//
 /* contenido LINEA DE TIEMPO  */
 const zoomInObserver = new IntersectionObserver((entries) => {
   entries.forEach(entry => {
@@ -251,10 +252,9 @@ const zoomInObserver = new IntersectionObserver((entries) => {
   threshold: 0.4 // Umbral al 80%
 });
 
-const zoomInContents = document.querySelectorAll('.nosotros_lt');
+
 zoomInContents.forEach(content => {
   content.style.opacity = '0';
   content.style.transform = 'scale(0.8)';
   zoomInObserver.observe(content);
 });
-
